5808d5aeac08dd435f58f10eeeddf6c09a4428d7
HermanMusyoki <salmantso14@gmail.com>
2023-08-10 10:15:37 -0700

    Use the function printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - the main funtion prints out a text
+ *
+ * Return: returs 0 after execution
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
