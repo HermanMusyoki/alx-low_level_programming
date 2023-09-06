a31fb93d814d05a4b2be59f8430aeabb01583e92
HermanMusyoki <salmantso14@gmail.com>
2023-09-06 12:12:48 -0700

    function that splits a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,52 +3,88 @@
 #include <string.h>
 
 /**
- * strtow - function that splits a string into words.
+ * strtow - a function that splits a string into two
  *
- * @str: The input string to be split.
+ * @str: the string to split
  *
- * Return: A pointer to argv and NULL (fail).
+ * Return: pointer to array of strings and NULL (fail)
  */
+
+int count_words(char *str);
+
 char **strtow(char *str)
 {
-	int m, n, p = 0;
+	int num_words;
+	char **words;
+	int word_start;
+	int word_end;
+	int word_length;
 	int word_count = 0;
-	char **words = NULL;
+	int i, j;
 
-	if (str == NULL || *str == '\0')
+	if (str == NULL || *str == '\0' || count_words(str) == 0)
 		return (NULL);
-	for (m = 0; str[m] != '\0'; m++)
-	{
-		if (str[m] != ' ' && (m == 0 || str[m - 1] == ' '))
-			word_count++;
-	}
-	words = malloc(sizeof(char *) * (word_count + 1));
+
+	num_words = count_words(str);
+	words = (char **)malloc((num_words + 1) * sizeof(char *));
 	if (words == NULL)
 		return (NULL);
-	for (m = 0; m <= word_count; m++)
-		words[m] = NULL;
-	for (m = 0; str[m] != '\0'; m++)
-	{
-		if (str[m] != ' ' && (m == 0 || str[m - 1] == ' '))
-		{
-			int word_length = 0;
 
-			for (n = m; str[n] != ' ' && str[n] != '\0'; n++)
-				word_length++;
-			words[p] = malloc(sizeof(char) * (word_length + 1));
-			if (words[p] == NULL)
+	word_start = 0;
+	word_end = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ')
+		{
+			word_start = i;
+			while (str[i] != ' ' && str[i] != '\0')
+				i++;
+			word_end = i;
+
+			word_length = word_end - word_start;
+			words[word_count] = (char *)malloc((word_length + 1) * sizeof(char));
+
+			if (words[word_count] == NULL)
 			{
-				for (n = 0; n < p; n++)
-					free(words[n]);
+				for (j = 0; j < word_count; j++)
+					free(words[j]);
 				free(words);
 				return (NULL);
 			}
-			for (n = 0; n < word_length; n++)
-				words[p][n] = str[m + n];
-			words[p][word_length] = '\0';
-			p++;
+			strncpy(words[word_count], &str[word_start], word_length);
+			words[word_count][word_length] = '\0';
+			word_count++;
 		}
 	}
-
+	words[word_count] = NULL;
 	return (words);
+}
+/**
+ * count_words - function that counts number of words in a string
+ *
+ * @str: the given string
+ *
+ * Return: words count
+ */
+
+int count_words(char *str)
+{
+	int count = 0;
+	int in_word = 0;
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ' && !in_word)
+		{
+			count++;
+			in_word = 1;
+		}
+		else if (str[i] == ' ')
+		{
+			in_word = 0;
+		}
+	}
+	return (count);
 }
